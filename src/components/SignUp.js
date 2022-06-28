e66d1dab4d15e2eaba3488263c9d01dd9137e201
Karan <dearkaransethi@gmail.com>
2022-06-28 12:03:30 +0530

    fixed some bugs

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -143,13 +143,15 @@ function SignUp() {
          >
              <GitHubIcon />
          </Button>
-
-         <Modal
+        {
+          
+          name?.length>=1 && (
+            <Modal
              open={open}
              onClose={handleClose}
              aria-labelledby="modal-modal-title"
              aria-describedby="modal-modal-description"
-        >
+              >
                <Box sx={style}>
                  <Typography id="modal-modal-title" variant="h6" component="h2">
                     {name} has signed in.
@@ -159,6 +161,8 @@ function SignUp() {
                  </Typography>
                </Box>
          </Modal>
+          )
+        }
          </div>
     </div>
   )
